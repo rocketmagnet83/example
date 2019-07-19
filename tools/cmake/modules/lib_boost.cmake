a6ac8a0dedfadc8f4978a70973b3cb36abc4039a
Steven Truppe <workbench@gmx.at>
2019-07-19 03:14:43 +0200

    Boost python with python3.7 (own compiled version) is working. the example just can't find the shared library but that's a problem for tomorrow =).

--- tools/cmake/modules/lib_boost.cmake
+++ tools/cmake/modules/lib_boost.cmake
@@ -4,16 +4,16 @@ if(WITH_LIB_BOOST)
 	set(LIB_BOOST_INC_PATH ${OUTPUT_PATH}/libs/boost/include/)
 	set(LIB_BOOST_LIB_PATH ${OUTPUT_PATH}/libs/boost/lib)
 	set(LIB_BOOST_DEPS external_boost)
-	set(LIB_BOOST_STATIC_LIBS boost_python27)
+	set(LIB_BOOST_STATIC_LIBS boost_python37)
  
 	ExternalProject_Add(external_boost
 		PREFIX ${CMAKE_BINARY_DIR}/boost
 		URL ${BOOST_URL}
 		DOWNLOAD_DIR ${CMAKE_BINARY_DIR}/boost
 		URL_HASH MD5=${BOOST_HASH}
-		#		PATCH_COMMAND cd ${CMAKE_BINARY_DIR}/boost/src/external_boost/ && patch project-config.jam ${CMAKE_SOURCE_DIR}/tools/patches/boost_python3.7.patch
+		PATCH_COMMAND ${CMAKE_COMMAND} -E copy ${CMAKE_SOURCE_DIR}/tools/build-configurations/boost/user-config.jam ${CMAKE_BINARY_DIR}/boost/src/external_boost/user-config.jam
 		CONFIGURE_COMMAND cd ${CMAKE_BINARY_DIR}/boost/src/external_boost/ && 
-		./bootstrap.sh --prefix=${OUTPUT_PATH}/libs/boost/ --with-libraries=python --with-python=${CMAKE_BINARY_DIR}/libs/python/python3.7 --with-python-root=${CMAKE_BINARY_DIR}/libs/python --with-python-version=3.7
+		./bootstrap.sh --prefix=${OUTPUT_PATH}/libs/boost/ --with-libraries=python --with-python=python3.7
 		BUILD_COMMAND cd ${CMAKE_BINARY_DIR}/boost/src/external_boost/ && 
 					  ./b2
 		INSTALL_COMMAND cd ${CMAKE_BINARY_DIR}/boost/src/external_boost/ && ./bjam && ./bjam install
